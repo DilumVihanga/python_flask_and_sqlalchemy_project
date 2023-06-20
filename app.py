7ac75da88e1db1d501e7164e204d40b8d241e811
DilumVihanga <88120903+DilumVihanga@users.noreply.github.com>
2023-06-20 05:56:43 +0000

    Data display using for loop

--- app.py
+++ app.py
@@ -2,10 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'doctor',
+    'location': 'beruwala',
+    'salary': 'rs. 10,000'
+  },
+  {
+    'id': 1,
+    'title': 'sdbdb',
+    'location': 'bdvdvuwala',
+    'salary': 'rs. 40,000'
+  },
+  {
+    'id': 1,
+    'title': 'dosvsdvctor',
+    'location': 'bfffuwala',
+    'salary': 'rs. 80,000'
+  },
+]
+
 
 @app.route('/')
 def hello():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_n='MasH')
 
 
 if __name__ == "__main__":
