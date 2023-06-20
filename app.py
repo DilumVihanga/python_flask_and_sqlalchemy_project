d50026c36dfc49ebc07af931d671259d3121aada
DilumVihanga <88120903+DilumVihanga@users.noreply.github.com>
2023-06-20 06:48:43 +0000

    jsonify the data with api/jobs separate file (separate database)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -10,16 +10,15 @@ JOBS = [
     'salary': 'rs. 10,000'
   },
   {
-    'id': 1,
-    'title': 'sdbdb',
+    'id': 2,
+    'title': 'cricketer',
     'location': 'bdvdvuwala',
     'salary': 'rs. 40,000'
   },
   {
-    'id': 1,
-    'title': 'dosvsdvctor',
-    'location': 'bfffuwala',
-    'salary': 'rs. 80,000'
+    'id': 3,
+    'title': 'freelance',
+    'location': 'remote',
   },
 ]
 
@@ -27,6 +26,11 @@ JOBS = [
 @app.route('/')
 def hello():
   return render_template('home.html', jobs=JOBS, company_n='MasH')
+
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
